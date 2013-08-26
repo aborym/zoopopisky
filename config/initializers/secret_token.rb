eecb6438c76ae11a370bffbfe4f402a771fd4a3d
Filip Ambrož <aborym@aborym.cz>
2013-08-26 15:43:09 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Zoopopisky::Application.config.secret_key_base = '72ebb3a929b5b2947995c7692da0e2cdff6cb5040b964277b925ef69cdac54b7a88e5b6d0a19ae53c6b848dd7f0c914f1c1d82eb3bb651fa0f63b249eeebf9e6'
